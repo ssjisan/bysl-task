d2c7fee5132462ee05195ec39b96ebd2d5f1ef6f
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2022-02-01 20:02:26 +0600

    left part added

--- src/Components/HeroSection/Components/LeftPart.js
+++ src/Components/HeroSection/Components/LeftPart.js
@@ -1,14 +1,31 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import React from "react";
-import useStyles from "./Style/LeftPartStyle"
+import useStyles from "./Style/LeftPartStyle";
+import { BsArrowRightCircle } from "react-icons/bs";
 
 export default function LeftPart() {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
     <Box>
       <Typography className={classes.header}>
         Tell a better brand story
       </Typography>
+      <Typography style={{ marginTop: "20px" }} variant="h6">
+        Automate the way you search through hashtags and suggested profiles to
+        find results 100x faster.
+      </Typography>
+      <Button
+        style={{
+          marginTop: "40px",
+          backgroundColor: "#002855",
+          borderRadius: "50px",
+          fontSize: "16px",
+        }}
+        variant="contained"
+      >
+        {" "}
+        Request Demo &nbsp; <BsArrowRightCircle/>
+      </Button>
     </Box>
   );
 }
--- src/Components/HeroSection/Components/Style/LeftPartStyle.js
+++ src/Components/HeroSection/Components/Style/LeftPartStyle.js
@@ -14,7 +14,7 @@ export default makeStyles((theme) => ({
     fontSize: "96px",
     fontWeight: "bold",
     color: "#f35626",
-    backgroundImage: " -webkit-linear-gradient(92deg, #f35626, #feab3a)",
+    backgroundImage: "linear-gradient(90deg, rgba(183,9,76,1) 0%, rgba(137,43,100,1) 43%, rgba(0,40,85,1) 76%, rgba(0,24,69,1) 100%)",
     "-webkit-background-clip": "text",
     "-webkit-text-fill-color": "transparent",
     "-webkit -animation": "hue 10s infinite linear",

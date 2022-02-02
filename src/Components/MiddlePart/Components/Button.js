553079fab95b4c06c989fdb86a7cf80d6a366607
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2022-02-02 19:55:49 +0600

    card section with dynamic color and text

--- src/Components/MiddlePart/Components/Button.js
+++ src/Components/MiddlePart/Components/Button.js
@@ -5,17 +5,17 @@ import { IoIosArrowForward } from "react-icons/io";
 export default function Button() {
   return (
     <Box>
-      <Box style={{ display: "flex", alignItems: "center" }}>
+      <Box style={{ display: "flex", alignItems: "center",marginBottom: "15px" }}>
         <IoIosArrowForward />
         <Typography>Get Started</Typography>
       </Box>
-      <Box style={{ display: "flex", alignItems: "center" }}>
+      <Box style={{ display: "flex", alignItems: "center",marginBottom: "15px" }}>
         <IoIosArrowForward />
         <Typography>Team on board</Typography>
       </Box>
-      <Typography style={{ marginLeft: "15px" }}>Case Study</Typography>
-      <Typography style={{ marginLeft: "15px" }}>Use case</Typography>
-      <Typography style={{ marginLeft: "15px" }}>Wow factor</Typography>
+      <Typography style={{ margin: "15px" }}>Case Study</Typography>
+      <Typography style={{ margin: "15px" }}>Use case</Typography>
+      <Typography style={{ margin: "15px" }}>Wow factor</Typography>
     </Box>
   );
 }
--- src/Components/MiddlePart/Components/CardContent.js
+++ src/Components/MiddlePart/Components/CardContent.js
@@ -7,119 +7,191 @@ import {
   Skeleton,
   Typography,
 } from "@mui/material";
-import React from "react";
-import { FaFacebookF } from "react-icons/fa";
-
+import React, { useState } from "react";
+import { FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import data from "../../../Assets/fakeData.json";
 export default function CardContent() {
+  const [userData, setUserData] = useState(data);
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item lg={4}>
-          <Card
-            style={{
-              background: " #FFFFFF",
-              boxShadow: "-4px 4px 18px rgba(34, 82, 155, 0.1)",
-              borderRadius: "20px",
-              padding: "5%",
-            }}
-          >
-            <Box
+        {userData.map((data) => (
+          <Grid item lg={4}>
+            <Card
               style={{
-                padding: "8px 68px",
-                borderRadius: "10px",
-                backgroundColor: "#FF9E00",
-                color: "white",
+                background: " #FFFFFF",
+                boxShadow: "-4px 4px 18px rgba(34, 82, 155, 0.1)",
+                borderRadius: "20px",
+                padding: "5%",
               }}
             >
-              <Typography>Product Updates</Typography>
-            </Box>
-            <Box
-              style={{
-                marginTop: "21px",
-                display: "flex",
-                flexDirection: "row",
-              }}
-            >
-              <Avatar
-                src="https://i.ibb.co/KxJKhVf/developer2-1.png"
-                style={{ width: 56, height: 56, marginRight: "20px" }}
-              />
-              <Box>
-                <Typography style={{ fontWeight: "bold" }}>
-                  User Name
+              <Box
+                style={{
+                  padding: "8px 68px",
+                  borderRadius: "10px",
+                  backgroundColor:
+                    (data.id === 1 && "#FF9E00") ||
+                    (data.id === 2 && "#06D6A0") ||
+                    (data.id === 3 && "#023E8A") ||
+                    (data.id === 4 && "#F72585") ||
+                    (data.id === 5 && "#9D4EDD") ||
+                    (data.id === 6 && "#1E96FC"),
+                  color: "white",
+                }}
+              >
+                <Typography style={{ fontSize: "16px", fontWeight: 600 }}>
+                  Product Updates
                 </Typography>
-                <Typography>Sub-Title</Typography>
               </Box>
-            </Box>
-            <Box style={{ marginTop: "40px" }}>
-              <Grid container>
-                <Grid item lg={6}>
-                  <IconButton
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      width: 46,
-                      height: 46,
-                      background:
-                        "linear-gradient(197.71deg, #243FE1 6.64%, #2F53E5 20.96%, #4C87ED 50.31%, #7BDAFB 91.77%, #84EAFE 99.8%)",
-                    }}
-                  >
-                    <FaFacebookF style={{ color: "white", fontSize: 24 }} />
-                  </IconButton>
-                </Grid>
-                <Grid item lg={6}>
-                  <Box
-                    style={{
-                      backgroundColor: "#E9EBF6",
-                      borderRadius: "10px",
-                      padding: "5%",
-                    }}
-                  >
-                    <Grid container justifyContent="center" alignItems="center">
+              <Box
+                style={{
+                  marginTop: "21px",
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <Avatar
+                  src="https://i.ibb.co/KxJKhVf/developer2-1.png"
+                  style={{ width: 48, height: 48, marginRight: "20px" }}
+                />
+                <Box>
+                  <Typography style={{ fontWeight: "bold", fontSize: "14px" }}>
+                    User Name
+                  </Typography>
+                  <Typography style={{ fontSize: "12px" }}>
+                    Sub-Title
+                  </Typography>
+                </Box>
+              </Box>
+              <Box style={{ marginTop: "30px" }}>
+                <Grid container justifyContent="flex-end" alignItems="flex-end">
+                  <Grid item lg={6}>
+                    <Grid
+                      container
+                      justifyContent="center"
+                      alignItems="center"
+                      spacing={2}
+                    >
                       <Grid item lg={4}>
                         {" "}
-                        <Skeleton
-                          animation="wave"
-                          variant="circular"
-                          width={40}
-                          height={40}
-                        />
+                        <Box
+                          style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: 46,
+                            height: 46,
+                            borderRadius: "23px",
+                            boxShadow:
+                              ((data.id === 1 || data.id === 6) &&
+                                "3px 6px 15px 5px rgba(87, 153, 239, 0.39)") ||
+                              ((data.id === 2 || data.id === 4) &&
+                                "3px 6px 15px 5px rgba(173, 105, 240, 0.6)") ||
+                              ((data.id === 3 || data.id === 5) &&
+                                "3px 6px 15px 5px rgba(59, 211, 14, 0.25)"),
+                            background:
+                              ((data.id === 1 || data.id === 6) &&
+                                "linear-gradient(197.71deg, #243FE1 6.64%, #2F53E5 20.96%, #4C87ED 50.31%, #7BDAFB 91.77%, #84EAFE 99.8%)") ||
+                              ((data.id === 2 || data.id === 4) &&
+                                "linear-gradient(202.41deg, #3A55D2 10.2%, #4651D5 21.3%, #6548DE 41.17%, #FF007A 95.07%)") ||
+                              ((data.id === 3 || data.id === 5) &&
+                                "linear-gradient(18.81deg, #6BDC4A 5.65%, #009A73 90.77%)"),
+                          }}
+                        >
+                          {(data.id === 1 || data.id === 6) && (
+                            <FaFacebookF
+                              style={{ color: "white", fontSize: 24 }}
+                            />
+                          )}
+                          {(data.id === 2 || data.id === 4) && (
+                            <FaInstagram
+                              style={{ color: "white", fontSize: 24 }}
+                            />
+                          )}
+                          {(data.id === 3 || data.id === 5) && (
+                            <FaWhatsapp
+                              style={{ color: "white", fontSize: 24 }}
+                            />
+                          )}
+                        </Box>
                       </Grid>
                       <Grid item lg={8}>
-                        {" "}
                         <Box>
                           <Skeleton
                             animation="wave"
                             height={10}
+                            width="60%"
                             style={{ marginBottom: 6 }}
                           />
                           <Skeleton
                             animation="wave"
                             height={10}
+                            width="80%"
                             style={{ marginBottom: 6 }}
                           />
                         </Box>
                       </Grid>
-                      <Grid item lg={12}>
-                        <Typography
-                          style={{
-                            padding: "5%",
-                            fontSize: "10px",
-                            fontWeight: 500,
-                            color: "white",
-                          }}
-                        >
-                          52 minutes ago
-                        </Typography>
-                      </Grid>
                     </Grid>
-                  </Box>
+                  </Grid>
+                  <Grid item lg={6}>
+                    <Box
+                      style={{
+                        backgroundColor: "#E9EBF6",
+                        borderRadius: "10px",
+                        padding: "5%",
+                      }}
+                    >
+                      <Grid
+                        container
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Grid item lg={4}>
+                          <Skeleton
+                            animation="wave"
+                            variant="circular"
+                            width={40}
+                            height={40}
+                          />
+                        </Grid>
+                        <Grid item lg={8}>
+                          {" "}
+                          <Box>
+                            <Skeleton
+                              animation="wave"
+                              height={10}
+                              width="60%"
+                              style={{ marginBottom: 6 }}
+                            />
+                            <Skeleton
+                              animation="wave"
+                              height={10}
+                              width="80%"
+                              style={{ marginBottom: 6 }}
+                            />
+                          </Box>
+                        </Grid>
+                        <Grid item lg={12}>
+                          <Typography
+                            style={{
+                              padding: "5%",
+                              fontFamily: "poppins",
+                              fontSize: "10px",
+                              fontWeight: 500,
+                              color: "white",
+                            }}
+                          >
+                            52 minutes ago
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Box>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Box>
-          </Card>
-        </Grid>
+              </Box>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
